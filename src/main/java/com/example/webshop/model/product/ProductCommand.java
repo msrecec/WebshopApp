e3d266d26631862a987a00f7a81147d5b8a58815
msrecec <67606889+msrecec@users.noreply.github.com>
2021-08-28 00:44:38 +0200

    added more input validation

--- src/main/java/com/example/webshop/model/product/ProductCommand.java
+++ src/main/java/com/example/webshop/model/product/ProductCommand.java
@@ -22,9 +22,12 @@ public class ProductCommand {
     @NotBlank(message = "Product name must not be blank")
     @Size(min = 1, max = 100, message = "Product name length must be between 1 and 100 characters (including 1 and 100)")
     private String name;
-    @NotNull
+    @NotNull(message = "Product price must not be null")
     @PositiveOrZero()
     private BigDecimal priceHrk;
+    @NotNull(message = "Product description must not be null")
+    @NotBlank(message = "Product description must not be blank")
     private String description;
+    @NotNull(message = "Product availability must not be null")
     private Boolean isAvailable;
 }
