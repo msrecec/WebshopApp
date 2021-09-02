1e8ee7e8e752b92afd42eb019e8a6fd3fbf3739e
msrecec <67606889+msrecec@users.noreply.github.com>
2021-09-02 23:25:17 +0200

    added controller unit tests

--- src/main/java/com/example/webshop/repository/hnbAPI/HnbRepositoryImpl.java
+++ src/main/java/com/example/webshop/repository/hnbAPI/HnbRepositoryImpl.java
@@ -5,6 +5,7 @@ import com.example.webshop.model.hnb.Hnb;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.SerializationUtils;
@@ -17,12 +18,18 @@ import java.util.Optional;
 @Repository
 public class HnbRepositoryImpl implements HnbRepository {
 
+    private final ObjectMapper objectMapper;
+
+    @Autowired
+    public HnbRepositoryImpl(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
     @Override
     public Optional<Hnb> findByCurrency(Currency currency, String resource) {
         RestTemplate restTemplate = new RestTemplate();
         try {
             ResponseEntity<String> response = restTemplate.getForEntity(resource+currency.getCurrency(), String.class);
-            ObjectMapper objectMapper = new ObjectMapper();
             Hnb[] hnb = objectMapper.readValue(response.getBody(), Hnb[].class);
             return Optional.ofNullable(hnb[0]);
         } catch (RestClientException | JsonProcessingException ex) {
--- src/test/java/com/example/webshop/rest/ProductControllerTest.java
+++ src/test/java/com/example/webshop/rest/ProductControllerTest.java
@@ -1,15 +1,23 @@
 package com.example.webshop.rest;
 
+import com.example.webshop.command.product.ProductCommand;
 import com.example.webshop.dto.product.ProductDTO;
 import com.example.webshop.service.product.ProductService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
@@ -18,6 +26,7 @@ import java.util.Arrays;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -28,25 +37,41 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @AutoConfigureMockMvc
 @SpringBootTest
+@TestPropertySource(
+        locations = "classpath:application-test.properties"
+)
 class ProductControllerTest {
 
     @Autowired
     MockMvc mockMvc;
 
-    @MockBean
+    @MockBean(name = "productService")
     ProductService productService;
 
+    @Autowired
+    ObjectMapper mapper;
+
     ProductDTO productDTO;
+    ProductCommand command;
 
     @BeforeEach
     void setUp() {
         this.productDTO = ProductDTO.builder()
-                .code("test")
+                .code("1234567890")
                 .isAvailable(true)
                 .name("test")
                 .description("test")
                 .priceHrk(new BigDecimal(100))
                 .build();
+
+        this.command = ProductCommand.builder()
+                .code("1234567890")
+                .isAvailable(true)
+                .name("test")
+                .description("test")
+                .priceHrk(new BigDecimal(100))
+                .build();
+
     }
 
 
@@ -121,14 +146,86 @@ class ProductControllerTest {
     }
 
     @Test
-    void save() {
+    void saveExistsTest() throws Exception {
+
+        when(productService.save(Mockito.any())).thenReturn(Optional.of(productDTO));
+
+        this.mockMvc.perform(
+                        post("/api/v1/product")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(mapper.writeValueAsString(command))
+                                .accept(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isCreated())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.code").value("1234567890"))
+                .andExpect(jsonPath("$.isAvailable").value(true))
+                .andExpect(jsonPath("$.name").value("test"))
+                .andExpect(jsonPath("$.description").value("test"))
+                .andExpect(jsonPath("$.priceHrk").value(new BigDecimal(100)));
+
+    }
+
+
+    @Test
+    void saveNotExistsTest() throws Exception {
+
+        when(productService.save(Mockito.any())).thenReturn(Optional.empty());
+
+        this.mockMvc.perform(
+                        post("/api/v1/product")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(mapper.writeValueAsString(command))
+                                .accept(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isConflict());
+
     }
 
     @Test
-    void update() {
+    void updateExistsTest() throws Exception {
+
+        when(productService.update(Mockito.any())).thenReturn(Optional.of(productDTO));
+
+        this.mockMvc.perform(
+                        put("/api/v1/product")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(mapper.writeValueAsString(command))
+                                .accept(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.code").value("1234567890"))
+                .andExpect(jsonPath("$.isAvailable").value(true))
+                .andExpect(jsonPath("$.name").value("test"))
+                .andExpect(jsonPath("$.description").value("test"))
+                .andExpect(jsonPath("$.priceHrk").value(new BigDecimal(100)));
     }
 
     @Test
-    void delete() {
+    void updateNotExistsTest() throws Exception {
+
+        when(productService.update(Mockito.any())).thenReturn(Optional.empty());
+
+        this.mockMvc.perform(
+                        put("/api/v1/product")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(mapper.writeValueAsString(command))
+                                .accept(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isConflict());
+    }
+
+
+
+    @Test
+    void delete() throws Exception {
+
+        this.mockMvc.perform(
+                MockMvcRequestBuilders.delete("/api/v1/product/code/{code}", "test")
+                ).andExpect(status().isNoContent());
+
+        verify(productService).deleteByCode("test");
+
     }
 }
