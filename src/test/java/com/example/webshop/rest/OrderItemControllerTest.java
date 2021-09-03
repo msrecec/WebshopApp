5b092771d48b8c4cfaed9231dcdcacf04fe8bc03
msrecec <67606889+msrecec@users.noreply.github.com>
2021-09-03 11:29:33 +0200

    added orderItem unit tests

--- src/test/java/com/example/webshop/rest/OrderItemControllerTest.java
+++ src/test/java/com/example/webshop/rest/OrderItemControllerTest.java
@@ -5,10 +5,12 @@ import com.example.webshop.command.orderItem.OrderItemPutCommand;
 import com.example.webshop.dto.orderItem.OrderItemDTO;
 import com.example.webshop.dto.product.ProductDTO;
 import com.example.webshop.service.orderItem.OrderItemService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -24,11 +26,14 @@ import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -60,8 +65,8 @@ class OrderItemControllerTest {
 
     @BeforeEach
     void setUp() {
-        orderItemPostCommand = OrderItemPostCommand.builder().orderId(1L).code("test").quantity(1).build();
-        orderItemPutCommand = OrderItemPutCommand.builder().id(1L).code("test").quantity(1).build();
+        orderItemPostCommand = OrderItemPostCommand.builder().orderId(1L).code("1234567890").quantity(1).build();
+        orderItemPutCommand = OrderItemPutCommand.builder().id(1L).code("1234567890").quantity(1).build();
         orderItemDTO = OrderItemDTO.builder()
                 .id(1L)
                 .quantity(1)
@@ -75,7 +80,7 @@ class OrderItemControllerTest {
     }
 
     @Test
-    void findAllTest() throws Exception {
+    void findAllExistsTest() throws Exception {
 
         List<OrderItemDTO> orderItemDTOList = new ArrayList<>();
 
@@ -99,18 +104,144 @@ class OrderItemControllerTest {
     }
 
     @Test
-    void findById() {
+    void findAllNotExistsTest() throws Exception {
+
+        // given
+
+        when(orderItemService.findAll()).thenReturn(new ArrayList<>());
+
+        // when
+
+        // then
+
+        this.mockMvc.perform(
+                        get("/api/v1/orderItem")
+                )
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$", hasSize(0)));
+
     }
 
     @Test
-    void update() {
+    void findByIdExistsTest() throws Exception {
+
+        when(orderItemService.findById(1L)).thenReturn(Optional.of(orderItemDTO));
+
+        this.mockMvc.perform(
+                        get("/api/v1/orderItem/id/{id}", 1L)
+                )
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.quantity").value(1))
+                .andExpect(jsonPath("$.product.code").value("1234567890"))
+                .andExpect(jsonPath("$.product.isAvailable").value(true))
+                .andExpect(jsonPath("$.product.name").value("test"))
+                .andExpect(jsonPath("$.product.description").value("test"))
+                .andExpect(jsonPath("$.product.priceHrk").value(new BigDecimal(100)));
+
     }
 
     @Test
-    void save() {
+    void findByIdNotExistsTest() throws Exception {
+
+        when(orderItemService.findById(1L)).thenReturn(Optional.empty());
+
+        this.mockMvc.perform(
+                        get("/api/v1/orderItem/id/{id}", 1L)
+                )
+                .andExpect(status().isNotFound());
+
     }
 
     @Test
-    void deleteById() {
+    void updateExistsTest() throws Exception {
+
+        when(orderItemService.update(Mockito.any())).thenReturn(Optional.of(orderItemDTO));
+
+        this.mockMvc.perform(
+                        put("/api/v1/orderItem")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(mapper.writeValueAsString(orderItemPutCommand))
+                                .accept(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.quantity").value(1))
+                .andExpect(jsonPath("$.product.code").value("1234567890"))
+                .andExpect(jsonPath("$.product.isAvailable").value(true))
+                .andExpect(jsonPath("$.product.name").value("test"))
+                .andExpect(jsonPath("$.product.description").value("test"))
+                .andExpect(jsonPath("$.product.priceHrk").value(new BigDecimal(100)));
+
+    }
+
+    @Test
+    void updateNotExistsTest() throws Exception {
+
+        when(orderItemService.update(Mockito.any())).thenReturn(Optional.empty());
+
+        this.mockMvc.perform(
+                        put("/api/v1/orderItem")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(mapper.writeValueAsString(orderItemPutCommand))
+                                .accept(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isConflict());
+
+    }
+
+    @Test
+    void saveExistsTest() throws Exception {
+
+
+        when(orderItemService.save(Mockito.any())).thenReturn(Optional.of(orderItemDTO));
+
+        this.mockMvc.perform(
+                        post("/api/v1/orderItem")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(mapper.writeValueAsString(orderItemPostCommand))
+                                .accept(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isCreated())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.quantity").value(1))
+                .andExpect(jsonPath("$.product.code").value("1234567890"))
+                .andExpect(jsonPath("$.product.isAvailable").value(true))
+                .andExpect(jsonPath("$.product.name").value("test"))
+                .andExpect(jsonPath("$.product.description").value("test"))
+                .andExpect(jsonPath("$.product.priceHrk").value(new BigDecimal(100)));
+
+    }
+
+
+    @Test
+    void saveNotExistsTest() throws Exception {
+
+        when(orderItemService.save(Mockito.any())).thenReturn(Optional.empty());
+
+        this.mockMvc.perform(
+                        post("/api/v1/orderItem")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(mapper.writeValueAsString(orderItemPostCommand))
+                                .accept(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isConflict());
+
+    }
+
+    @Test
+    void deleteById() throws Exception {
+
+
+        this.mockMvc.perform(
+                MockMvcRequestBuilders.delete("/api/v1/orderItem/id/{id}", 1L)
+        ).andExpect(status().isNoContent());
+
+        verify(orderItemService).deleteById(1L);
+
     }
 }
