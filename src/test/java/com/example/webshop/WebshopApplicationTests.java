0e7c4a0cce1d8965d68886d9b0efa22fef74e39e
msrecec <67606889+msrecec@users.noreply.github.com>
2021-09-04 19:11:39 +0200

    quickfix

--- src/test/java/com/example/webshop/WebshopApplicationTests.java
+++ src/test/java/com/example/webshop/WebshopApplicationTests.java
@@ -2,12 +2,14 @@ package com.example.webshop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @TestPropertySource(
 		locations = "classpath:application-test.properties"
 )
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class WebshopApplicationTests {
 
 	@Test
