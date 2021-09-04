ca1ccb4e63c12627f5f2e7ac2da64a8e14083ae5
msrecec <67606889+msrecec@users.noreply.github.com>
2021-09-04 16:28:22 +0200

    updated context test

--- src/test/java/com/example/webshop/WebshopApplicationTests.java
+++ src/test/java/com/example/webshop/WebshopApplicationTests.java
@@ -2,8 +2,12 @@ package com.example.webshop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(
+		locations = "classpath:application-test.properties"
+)
 class WebshopApplicationTests {
 
 	@Test
