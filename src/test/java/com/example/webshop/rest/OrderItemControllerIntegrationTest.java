9debf47d38a0b42e21fd72917c988b5846974197
msrecec <67606889+msrecec@users.noreply.github.com>
2021-09-04 13:18:36 +0200

    added OrderItemIntegrationTest

--- src/test/java/com/example/webshop/rest/OrderItemControllerIntegrationTest.java
+++ src/test/java/com/example/webshop/rest/OrderItemControllerIntegrationTest.java
@@ -9,6 +9,7 @@ import com.example.webshop.service.orderItem.OrderItemService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,10 +18,13 @@ import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -50,8 +54,8 @@ public class OrderItemControllerIntegrationTest {
 
     @BeforeEach
     void setUp() {
-        orderItemPostCommand = OrderItemPostCommand.builder().orderId(1L).code("1234567891").quantity(1).build();
-        orderItemPutCommand = OrderItemPutCommand.builder().id(1L).code("1234567890").quantity(1).build();
+        orderItemPostCommand = OrderItemPostCommand.builder().orderId(1L).code("1234567899").quantity(2).build();
+        orderItemPutCommand = OrderItemPutCommand.builder().id(1L).code("1234567890").quantity(2).build();
     }
 
 
@@ -70,6 +74,92 @@ public class OrderItemControllerIntegrationTest {
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$", hasSize(2)));
+
+    }
+
+
+    @Test
+    void findByIdExistsIT() throws Exception {
+
+        this.mockMvc.perform(
+                        get("/api/v1/orderItem/id/{id}", 1L)
+                )
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.quantity").value(1))
+                .andExpect(jsonPath("$.product.code").value("1234567890"))
+                .andExpect(jsonPath("$.product.isAvailable").value(true))
+                .andExpect(jsonPath("$.product.name").value("Great product"))
+                .andExpect(jsonPath("$.product.description").value("This is a great product"));
+
+    }
+
+
+    @Test
+    void updateExistsIT() throws Exception {
+
+        this.mockMvc.perform(
+                        put("/api/v1/orderItem")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(mapper.writeValueAsString(orderItemPutCommand))
+                                .accept(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.quantity").value(2))
+                .andExpect(jsonPath("$.product.code").value("1234567890"))
+                .andExpect(jsonPath("$.product.isAvailable").value(true))
+                .andExpect(jsonPath("$.product.name").value("Great product"))
+                .andExpect(jsonPath("$.product.description").value("This is a great product"));
+
+    }
+
+    @Test
+    void saveExistsIT() throws Exception {
+
+        // given
+        this.mockMvc.perform(
+                        MockMvcRequestBuilders.delete("/api/v1/orderItem/id/{id}", 2L)
+                )
+                .andExpect(status().isNoContent());
+
+        // when
+
+        // then
+
+        this.mockMvc.perform(
+                        post("/api/v1/orderItem")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(mapper.writeValueAsString(orderItemPostCommand))
+                                .accept(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isCreated())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id").value(3))
+                .andExpect(jsonPath("$.quantity").value(2))
+                .andExpect(jsonPath("$.product.code").value("1234567899"))
+                .andExpect(jsonPath("$.product.isAvailable").value(true))
+                .andExpect(jsonPath("$.product.name").value("Another Great product"))
+                .andExpect(jsonPath("$.product.description").value("This is an another great product"));
+
+    }
+
+    @Test
+    void deleteByIdIT() throws Exception {
+
+
+        this.mockMvc.perform(
+                MockMvcRequestBuilders.delete("/api/v1/orderItem/id/{id}", 1L)
+        ).andExpect(status().isNoContent());
+
+        this.mockMvc.perform(
+                        get("/api/v1/orderItem")
+                )
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$", hasSize(1)));
 
     }
 
